622bde78c61ee37385c1b81860bd01c19acb700f
Jesús Fonseca Serrano <jesusfonseruma@gmail.com>
2025-10-29 22:56:58 +0100

    Implement additional Square components for current turn

--- src/App.jsx
+++ src/App.jsx
@@ -8,37 +8,33 @@ const TURNS = {
   O: "o",
 };
 
+const Square = ({ children, updateBoard, index, isSelected }) => {
+  const className = `square ${isSelected ? "is-selected" : "" }`
 
-const Square = ({children,  updateBoard, index}) =>{
-  return (
-    <div className="square">
-      {children}
-    </div>
-  )
-}
+  return <div className={className}>{children}</div>;
+};
 
 function App() {
-
   const [board, setBoard] = useState(Array(9).fill(null));
   const [turn, setTurn] = useState(TURNS.X);
 
   return (
-    <main className='board'>
+    <main className="board">
       <h1>Tic Tac Toe</h1>
       <section className="game">
-        {
-          board.map(( _,index) =>{
-            return (
-             <Square key={index}
-             index={index}
-             >
+        {board.map((_, index) => {
+          return (
+            <Square key={index} index={index}>
               {board[index]}
-             </Square>
-            )
-          })
-        }
+            </Square>
+          );
+        })}
       </section>
 
+      <section className="turn">
+        <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
+        <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
+      </section>
     </main>
   );
 }
