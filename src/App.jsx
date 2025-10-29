39217ea5f155626c9fb5a301667bc482e1292db8
Jesús Fonseca Serrano <jesusfonseruma@gmail.com>
2025-10-29 21:54:50 +0100

    Paint the board

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,43 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { useState } from "react";
+
+const TURNS = {
+  X: "x",
+  O: "o",
+};
 
 
-function App() {
-
+const Square = ({children,  updateBoard, index}) =>{
   return (
-    <h1>Tic Tac Toe</h1>
+    <div className="square">
+      {children}
+    </div>
   )
 }
 
-export default App
+function App() {
+
+
+  return (
+    <main className='board'>
+      <h1>Tic Tac Toe</h1>
+      <section className="game">
+        {
+          board.map(( _,index) =>{
+            return (
+             <Square key={index}
+             index={index}
+             >
+              
+             </Square>
+            )
+          })
+        }
+      </section>
+    </main>
+  );
+}
+
+export default App;
